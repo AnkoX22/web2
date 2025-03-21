7d171fcfbf4dd167090e8d27085e947adae0e19b
CHANKO XHEMAJ <128543515+AnkoX22@users.noreply.github.com>
2025-03-21 15:27:35 +0200

    Update authanticator.js

--- authanticator.js
+++ authanticator.js
@@ -4,6 +4,8 @@ let users = [
     {username: "user2", password: "user2password"}
 ];
 
+let newUsers = [];
+
 function authanticatePassword(username, password) {
 
     username = String(username).trim();
@@ -40,11 +42,54 @@ function handleSubmit(event) {
     }
 }
 
+function handleSignUp(event){
+
+    event.preventDefault();
+
+    const new_username = document.getElementById("username").value;
+    const new_password = document.getElementById("password").value;
+    const first_name = document.getElementById("firstname").value;
+    const last_name = document.getElementById("lastname").value;
+    const  email = document.getElementById("email").value;
+    const favourite_books = document.getElementById("favouritebooks").value;
+
+    const new_user = {
+        username: String(new_username).trim(),
+        password: String(new_password).trim(),
+        first_name: String(first_name).trim(),
+        last_name: String(last_name).trim(),
+        email: String(email).trim(),
+        favourite_books: String(favourite_books),
+    }
+
+    if(new_username && new_password && first_name && last_name && email && favourite_books){
+        window.location.href = "main.html";
+    } else {
+        const error_message = document.getElementById("error-message");
+
+        error_message.textContent = "Yu haven't filled all the input values"
+        error_message.style.display = "block";
+        error_message.style.color = "red";
+    }
+
+    newUsers.push(new_user);
+    users.push({username: new_username, password: new_password})
+
+    console.log(newUsers);
+    console.log(users);
+}
+
 document.addEventListener("DOMContentLoaded", function(){
 
     const form = document.getElementById("login-form");
+    const sign_up_form = document.getElementById("signup-form")
 
     if(form){
         form.addEventListener("submit",handleSubmit);
     }
+
+    if(sign_up_form){
+        sign_up_form.addEventListener("submit", handleSignUp);
+    }
 });
+
